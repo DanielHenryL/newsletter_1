0ee87e90e956c1026e539e7602049b3ffadb1ba0
HenryDani <lagunasdaniel001@gmail.com>
2022-06-29 00:11:14 -0500

    Creando las vistapara dashboard

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path('newsletter/',include('newsletters.urls',namespace='newsletter'))
+    path('newsletter/',include('newsletters.urls',namespace='newsletter')),
+    path('dashboard/',include('dashboard.urls',namespace='dashboard'))
 ]
